f330163b215f6cf380306d8dd8b007665e8a36a5
Jason De Lara <jason.delara@mojavepayments.com>
2024-09-10 15:34:27 -0700

    basic electron vite config no typescript

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+// Use contextBridge
+window.ipcRenderer.on('main-process-message', (_event, message) => {
+	console.log(message)
+  })
+  
